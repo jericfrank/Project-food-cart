fe580a60aebcda4264413ea04f47cc9804b605f9
Jeric Frank Sasil <jericfrank@gmail.com>
2018-01-30 15:53:34 +0800

    refactor(App): layout and styles

---
+++ src/containers/App/css.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+import { Layout } from 'antd';
+
+export const LayoutWrapper = styled( Layout )`
+    min-height : 100vh;
+`;
+
+export const ContentWrapper = styled( Layout.Content )`
+    padding : 0 50px;
+`;
+
+export const Content = styled.div`
+    background : #fff;
+    padding    : 24px;
+    min-height : 280px;
+    margin     : 16px 0;
+`;
+
+export const FooterWrapper = styled( Layout.Footer )`
+    text-align : center;
+`;
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,18 +1,29 @@
+import 'antd/dist/antd.css';
+
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from 'containers/HomePage';
 import ContactPage from 'containers/ContactPage';
 
+import { LayoutWrapper, ContentWrapper, Content, FooterWrapper } from './css';
+
 class App extends Component {
     render() {
         return (
-            <div>
-                <Switch>
-                    <Route exact path='/' component={HomePage}/>
-                    <Route path='/contact' component={ContactPage}/>
-                </Switch>
-            </div>
+            <LayoutWrapper>
+                <ContentWrapper>
+                    <Content>
+                        <Switch>
+                            <Route exact path='/' component={HomePage}/>
+                            <Route path='/contact' component={ContactPage}/>
+                        </Switch>
+                    </Content>
+                </ContentWrapper>
+                <FooterWrapper>
+                    Reserve © 2018
+                </FooterWrapper>
+            </LayoutWrapper>
         );
     }
 }
