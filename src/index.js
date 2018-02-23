e00256a5c723fd35fe4388b02100ee62451f9d53
Jeric Frank Sasil <jericfrank@gmail.com>
2018-02-24 01:19:13 +0800

    feat(Redux): hoc react redux

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux';
 
 import registerServiceWorker from './registerServiceWorker';
 
 import './global-css.js';
 import App from 'containers/App';
+import reducer from './reducer';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+	<Provider store={createStoreWithMiddleware(reducer)}>
+		<BrowserRouter>
+	        <App />
+	    </BrowserRouter>
+	</Provider>,
     document.getElementById('root')
 );
 
