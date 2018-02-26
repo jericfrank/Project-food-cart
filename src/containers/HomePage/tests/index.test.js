597a9be5fc740e390fa657ebdc2407b809464210
Jeric Frank Sasil <jericfrank@gmail.com>
2018-02-27 01:52:28 +0800

    test(HomePage): renders HeaderWrapper and ItemWrapper components

--- src/containers/HomePage/tests/index.test.js
+++ src/containers/HomePage/tests/index.test.js
@@ -6,28 +6,18 @@ import HomePage from 'containers/HomePage';
 import HeaderWrapper from 'containers/HeaderWrapper';
 import ItemWrapper from 'containers/ItemWrapper';
 
-describe('<HomePage />' , () => {
-    it('renders without crashing', () => {
-        const div = document.createElement('div');
-        
-        ReactDOM.render(<HomePage />, div);
-        ReactDOM.unmountComponentAtNode(div);
+describe('<HomePage />', () => {
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<HomePage />);
     });
 
-    describe('renders children', () => {
-        let wrapper;
+    it('renders <HeaderWrapper /> component', () => {
+        expect( wrapper.find( HeaderWrapper ) ).toHaveLength( 1 );
+    });
 
-        beforeEach(() => {
-            wrapper = shallow(<HomePage />);
-        });
-
-        it('<HeaderWrapper />', () => {
-            expect( wrapper.find( HeaderWrapper ) ).toHaveLength( 1 );
-        });
-
-        it('<ItemWrapper />', () => {
-            expect( wrapper.find( ItemWrapper ) ).toHaveLength( 1 );
-        });
+    it('renders <ItemWrapper /> component', () => {
+        expect( wrapper.find( ItemWrapper ) ).toHaveLength( 1 );
     });
 });
-    
